5783d477e41ff62b2ac4ceead10c5b6d7431f08f
William <wemz17@gmail.com>
2016-06-13 04:01:53 -0500

    Iniciemos

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<header>
+	<h1>hola Mundo </h1>
+	</header>
+	<section>
+		<h1>Section</h1>
+	</section>
+	<footer><h3>Footer</h3></footer>
+</body>
+</html>
