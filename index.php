f7d362d2ae0ef43c9947214fb8bc0c9b9fdf41b4
William <wemz17@gmail.com>
2016-06-14 21:31:38 -0500

    Ultima modificacion

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
 	<header>
@@ -10,6 +11,33 @@
 	</header>
 	<section>
 		<h1>Section</h1>
+
+		<div class="test">probando.. y haciendo una modificacion en la pag probando Git</div>
+		<div class="test">probando.. y haciendo una modificacion en la pag probando Git</div>
+
+		<script>
+				var n = 2;
+var no = "no";
+while(n)
+{
+    no += no;
+    n -= 1;
+}
+
+
+		ript>
+	
+
+		<table>
+			<td>
+				<tr class="algo1">Test</tr>
+				<tr class="algo2"> proband</tr>
+			</td>
+			<td>
+				<tr class="algo1">otra</tr>
+				<tr class="algo2">cosa</tr>
+			</td>
+		</table>
 	</section>
 	<footer><h3>Footer</h3></footer>
 </body>
